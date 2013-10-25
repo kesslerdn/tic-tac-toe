62a4cbea25c6561caf5e4fa404cff9f45bfbf333
dkessler <dkessler@sourceallies.com>
2013-10-25 12:14:25 -0500

    added a second acceptance test scenario; refactored to make it more readable

--- src/test/groovy/org/kesslerdn/tictactoe/acceptance/SpringMatchTest.groovy
+++ src/test/groovy/org/kesslerdn/tictactoe/acceptance/SpringMatchTest.groovy
@@ -1,44 +1,77 @@
 package org.kesslerdn.tictactoe.acceptance
 
-import javax.annotation.Resource
-
+import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.kesslerdn.tictactoe.game.Match
 import org.kesslerdn.tictactoe.game.player.TestPlayer
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.context.support.ClassPathXmlApplicationContext
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:/test-context.xml")
+
 class SpringMatchTest extends GroovyTestCase {
 
-	@Resource
 	private Match match
-	
-	@Resource
 	private TestPlayer firstPlayer
 	
+	@Before
+	public void setUp(){
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("test-context.xml")
+		match = context.getBean(Match.class)
+		firstPlayer = context.getBean(TestPlayer.class)
+	}
+	
 	@Test
-	void testStart() {
+	void testStart_WithTopLeft() {
 		firstPlayer.testingPositions = ["1","5","4", "7"]
-		firstPlayer.expectedBoard = ['''|1|2|3|
-|4|5|6|
-|7|8|9|
-''', 
-'''|X|O|3|
-|4|5|6|
-|7|8|9|
-''', 
-'''|X|O|3|
-|4|X|6|
-|7|8|O|
-''', 
-'''|X|O|3|
-|X|X|O|
-|7|8|O|
-''']
+		firstPlayer.expectedBoard = [
+			expectedBoard(  "1","2","3",
+							"4","5","6",
+							"7","8","9"),
+						
+			expectedBoard(  "X","O","3",
+							"4","5","6",
+							"7","8","9"),
+						
+			expectedBoard(  "X","O","3",
+							"4","X","6",
+							"7","8","O"),
+ 
+			expectedBoard(  "X","O","3",
+							"X","X","O",
+							"7","8","O")]
 		
 		match.start();
+	}
+
+
+	@Test
+	void testStart_WithTopRight() {
+		firstPlayer.testingPositions = ["3","5","6", "9"]
+		firstPlayer.expectedBoard = [
+			expectedBoard(  "1","2","3",
+							"4","5","6",
+							"7","8","9"),
+						
+			expectedBoard(  "O","2","X",
+							"4","5","6",
+							"7","8","9"),
+						
+			expectedBoard(  "O","2","X",
+							"4","X","6",
+							"O","8","9"),
+ 
+			expectedBoard(  "O","2","X",
+							"O","X","X",
+							"O","8","9")]
+	
+		match.start();
+	}
+	
+	private String expectedBoard(String one, String two, String three, 
+		String four, String five, String six, 
+		String seven, String eight, String nine){
+		"""|$one|$two|$three|
+|$four|$five|$six|
+|$seven|$eight|$nine|
+"""
 	}
 }
