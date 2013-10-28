d467724221bd7aa6da1a44d82fa0822783c3d046
dkessler <dkessler@sourceallies.com>
2013-10-28 07:44:44 -0500

    created trial score calculator to calculate a score for each trial position within a row

--- src/main/groovy/org/kesslerdn/tictactoe/ai/strategy/exhaustive/TrialRowScoreCalculator.groovy
+++ src/main/groovy/org/kesslerdn/tictactoe/ai/strategy/exhaustive/TrialRowScoreCalculator.groovy
@@ -6,9 +6,24 @@ import org.springframework.stereotype.Component
 @Component
 class TrialRowScoreCalculator implements ScoreCalculator{
 
+	private static final int TWO_OPPOSING_MATCHES = -1
+	private static final int HIGHEST_SCORE = 4
 	@Override
 	public Integer calculate(List<Position> positions, Position trialPosition) {
-		return null;
+		Integer score = 0
+		Position matchingPosition = positions.find{it.index == trialPosition.index && it.mark == null}
+		if(matchingPosition){
+			score++
+			positions.each{position ->
+				if(position.mark == trialPosition.mark){
+					score++
+				}
+				if(position.mark && position.mark != trialPosition.mark){
+					score--
+				}
+			}
+		}
+		score == TWO_OPPOSING_MATCHES ? HIGHEST_SCORE : score
 	}
 
 }
---
+++ src/test/groovy/org/kesslerdn/tictactoe/ai/strategy/exhaustive/TrialRowScoreCalculatorTest.groovy
@@ -0,0 +1,63 @@
+package org.kesslerdn.tictactoe.ai.strategy.exhaustive
+
+import static org.junit.Assert.*
+import static org.mockito.Mockito.*
+import groovy.util.GroovyTestCase
+
+import java.util.List
+
+import org.junit.Before
+import org.junit.Test
+import org.kesslerdn.tictactoe.board.position.Position
+import org.kesslerdn.tictactoe.board.position.TestPosition
+import org.kesslerdn.tictactoe.game.player.Mark
+
+
+class TrialRowScoreCalculatorTest extends GroovyTestCase {
+
+	ScoreCalculator calculator
+	private static final Mark OPEN = null;
+	
+	@Before
+	void setUp(){
+		calculator = new TrialRowScoreCalculator();
+	}
+	
+	@Test
+	void testCalculate_EmptyRow(){
+		assert 1 == calculator.calculate(createRow(OPEN, OPEN, OPEN), createTrialPosition(1, Mark.O))
+	}
+	
+	@Test
+	void testCalculate_OneMatch(){
+		assert 2 == calculator.calculate(createRow(OPEN, Mark.O, OPEN), createTrialPosition(1, Mark.O))
+	}
+	
+	@Test
+	void testCalculate_TwoMatch(){
+		assert 3 == calculator.calculate(createRow(OPEN, Mark.O, Mark.O), createTrialPosition(1, Mark.O))
+	}
+	
+	@Test
+	void testCalculate_OneOfEach(){
+		assert 1 == calculator.calculate(createRow(OPEN, Mark.O, Mark.X), createTrialPosition(1, Mark.O))
+	}
+	
+	@Test
+	void testCalculate_TwoNonMatches(){
+		assert 4 == calculator.calculate(createRow(OPEN, Mark.X, Mark.X), createTrialPosition(1, Mark.O))
+	}
+
+	@Test
+	void testCalculate_TrialPositionNotInRow(){
+		assert 0 == calculator.calculate(createRow(OPEN, OPEN, OPEN), createTrialPosition(4, Mark.O))
+	}
+
+	private List<Position> createRow(Mark first, Mark second, Mark third){
+		[TestPosition.newInstance(1, first),TestPosition.newInstance(2, second),TestPosition.newInstance(3, third)]
+	}
+	
+	private TrialPosition createTrialPosition(Integer index, Mark mark){
+		new TrialPosition(index:index, mark: mark)
+	}
+}
