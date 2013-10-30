8504e189ca907e4e2fd283b62e1035df11fff775
dkessler <dkessler@sourceallies.com>
2013-10-30 05:36:03 -0500

    removed unused cumulative strategy; #15

--- src/main/groovy/org/kesslerdn/tictactoe/ai/strategy/cumulative/PositionStrategy.groovy
+++
@@ -1,9 +0,0 @@
-package org.kesslerdn.tictactoe.ai.strategy.cumulative
-
-import org.kesslerdn.tictactoe.ai.PositionCounter;
-import org.kesslerdn.tictactoe.board.Board;
-
-
-interface PositionStrategy {
-	PositionCounter addPositions(Board board, PositionCounter positionCounter)
-}
